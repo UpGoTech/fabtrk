3d1cb88427125e05cb86e1bf98e917aa17d7da4b
mrunali00 <mrunal200026@gmail.com>
2026-01-10 12:01:26 +0000

    update cutomer, month,year,project type

---
+++ fabtrk/fabtrk/doctype/ft__project_month/ft__project_month.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2026, UpGo Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("FT  Project Month", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+
+
+
+// frappe.ui.form.on('FT Project Month', {
+//     onload(frm) {
+//         frm.set_query('month', function () {
+//             return {
+//                 query: 'fabtrk.fabtrk.doctype.ft_month.ft_month.get_sorted_months_for_project'
+//             };
+//         });
+//     }
+// });
+
+
--- fabtrk/fabtrk/doctype/ft_month/ft_month.py
+++ fabtrk/fabtrk/doctype/ft_month/ft_month.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2026, UpGo Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
-class FXLProject(Document):
+class FTMonth(Document):
 	pass
+
+
+
