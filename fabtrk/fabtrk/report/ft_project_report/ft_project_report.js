92c2934aef7aed29c295c1a08112491b0533143c
mrunali00 <mrunal200026@gmail.com>
2026-01-22 08:12:48 +0000

    add percentage field and add filter for month field

--- fabtrk/fabtrk/report/ft_project_report/ft_project_report.js
+++ fabtrk/fabtrk/report/ft_project_report/ft_project_report.js
@@ -2,144 +2,6 @@
 // For license information, please see license.txt
 
 
-
-
-// frappe.query_reports["FT Project Report"] = {
-// 	filters: [
-// 		{
-// 			fieldname: "year",
-// 			label: __("Year"),
-// 			fieldtype: "Link",
-// 			options: "FT Year",
-// 			on_change() {
-// 				frappe.query_report.refresh();
-// 			}
-// 		},
-// 		{
-// 			fieldname: "project",
-// 			label: __("Project"),
-// 			fieldtype: "Link",
-// 			options: "FT Project",
-// 			on_change() {
-// 				frappe.query_report.refresh();
-// 			}
-// 		}
-// 	],
-
-// 	// formatter(value, row, column, data, default_formatter) {
-// 	//     if (column.fieldname === "view") {
-// 	//         return `<button class="btn btn-xs btn-primary view-btn"
-// 	//                 data-month="${data.month_raw}">
-// 	//                 View
-// 	//             </button>`;
-// 	//     }
-// 	//     return default_formatter(value, row, column, data);
-// 	// },
-// 	formatter(value, row, column, data, default_formatter) {
-// 		if (column.fieldname === "view") {
-// 			console.log("MONTH ID 👉", data.month_id);
-// 			return `
-//             <button class="btn btn-xs btn-primary view-btn"
-//                 data-month="${data.month_id}">
-//                 View
-//             </button>`;
-// 		}
-// 		return default_formatter(value, row, column, data);
-// 	}
-// 	,
-
-// 	onload() {
-// 		if (!$("#details_area").length) {
-// 			$('<div id="details_area" style="margin-top:20px;"></div>')
-// 				.appendTo(frappe.query_report.page.main);
-// 		}
-
-// 		$(document).on("click", ".view-btn", function () {
-// 			refresh_project_details($(this).data("month"));
-// 		});
-// 	}
-// };
-
-// // function refresh_project_details(month = null) {
-// // 	frappe.call({
-// // 		method: "testapp.testapp.report.individual_project_report.individual_project_report.get_month_details",
-// // 		args: {
-// // 			year: frappe.query_report.get_filter_value("year"),
-// // 			project: frappe.query_report.get_filter_value("project"),
-// // 			month: month
-// // 		},
-// // 		callback(r) {
-// // 			if (r.message?.length) {
-// // 				render_details(r.message);
-// // 			} else {
-// // 				$("#details_area").html("<b>No data found</b>");
-// // 			}
-// // 		}
-// // 	});
-// // }
-// function refresh_project_details(month) {
-// 	frappe.call({
-// 		method: "fabtrk.fabtrk.report.ft_project_report.ft_project_report.get_month_details",
-// 		args: {
-// 			year: frappe.query_report.get_filter_value("year"),
-// 			project: frappe.query_report.get_filter_value("project"),
-// 			month: month
-// 		},
-// 		callback(r) {
-// 			if (r.message && r.message.length) {
-// 				render_details(r.message);
-// 			} else {
-// 				$("#details_area").html("<b>No data found</b>");
-// 			}
-// 		}
-// 	});
-// }
-
-
-// function render_details(data) {
-// 	let html = `
-//         <div style="display:flex;justify-content:space-between">
-//             <h4>Project Details</h4>
-//             <button id="close_details" class="btn btn-xs btn-danger">Close</button>
-//         </div>
-//         <table class="table table-bordered">
-//             <thead>
-//                 <tr>
-//                     <th>Project</th>
-//                     <th>Target (Kg)</th>
-//                     <th>Achieved (Kg)</th>
-//                     <th>Balance</th>
-//                     <th>Invoice</th>
-//                     <th>Attach</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//     `;
-
-// 	data.forEach(d => {
-// 		let attach = d.attachment
-// 			? `<a href="${d.attachment}" target="_blank">Open</a>`
-// 			: "-";
-
-// 		html += `
-//             <tr>
-//                 <td>${d.project}</td>
-//                 <td>${d.target || 0}</td>
-//                 <td>${d.achieved || 0}</td>
-//                 <td>${d.balance || 0}</td>
-//                 <td>${d.invoice_no || ""}</td>
-//                 <td>${attach}</td>
-//             </tr>`;
-// 	});
-
-// 	html += "</tbody></table>";
-// 	$("#details_area").html(html).show();
-
-// 	$("#close_details").on("click", () => $("#details_area").hide());
-// }
-
-
-
 frappe.query_reports["FT Project Report"] = {
 	filters: [
 		{
@@ -254,6 +116,8 @@ function render_details(data) {
                 <td>${d.target_weight || 0}</td>
                 <td>${d.achieved_weight || 0}</td>
                 <td>${d.balance_weight || 0}</td>
+
+				<td>${d.target_weight ? ((d.achieved_weight / d.target_weight) * 100).toFixed(2) : 0}%</td>
 
                 <td>${d.achievement_type || ""}</td>
 
--- fabtrk/fabtrk/report/ft_project_report/ft_project_report.py
+++ fabtrk/fabtrk/report/ft_project_report/ft_project_report.py
@@ -9,49 +9,35 @@
 # 	return columns, data
 
 
+import frappe
+from frappe.utils import formatdate
 
+def execute(filters=None):
+    filters = filters or {}
+    year = filters.get("year")
+    project = filters.get("project")
 
+    columns = get_columns()
+    data = get_data(year, project)
 
-# # /////////////////15-01-25
-# import frappe
+    total_target = sum(d.get("target", 0) for d in data)
+    total_achieved = sum(d.get("achieved", 0) for d in data)
 
-# # def execute(filters=None):
-# #     filters = filters or {}
-# #     year = filters.get("year")
-# #     project = filters.get("project")
+    # summary = [
+    #     {"label": "Total Target (Kg)", "value": total_target},
+    #     {"label": "Total Achieved (Kg)", "value": total_achieved},
+    #     {"label": "Balance", "value": total_target - total_achieved}
+    # ]
+    achieved_percent = round((total_achieved / total_target) * 100, 2) if total_target else 0
 
-# #     columns = get_columns()
-# #     data = get_data(year, project)
-
-# #     total_target = sum(d.target for d in data)
-# #     total_achieved = sum(d.achieved for d in data)
-
-# #     summary = [
-# #         {"label": "Total Target (Kg)", "value": total_target},
-# #         {"label": "Total Achieved (Kg)", "value": total_achieved},
-# #         {"label": "Balance", "value": total_target - total_achieved}
-# #     ]
-
-# #     return columns, data, None, None, summary
-# def execute(filters=None):
-#     filters = filters or {}
-#     year = filters.get("year")
-#     project = filters.get("project")
-
-#     columns = get_columns()
-#     data = get_data(year, project)
-
-#     total_target = sum(d.get("target", 0) for d in data)
-#     total_achieved = sum(d.get("achieved", 0) for d in data)
-
-#     summary = [
-#         {"label": "Total Target (Kg)", "value": total_target},
-#         {"label": "Total Achieved (Kg)", "value": total_achieved},
-#         {"label": "Balance", "value": total_target - total_achieved}
-#     ]
-
-#     return columns, data, None, None, summary
-
+    summary = [
+        {"label": "Total Target (Kg)", "value": total_target},
+        {"label": "Total Achieved (Kg)", "value": total_achieved},
+        {"label": "Balance", "value": total_target - total_achieved},
+        {"label": "Achieved %", "value": achieved_percent}
+    ]
+    
+    return columns, data, None, None, summary
 
 # def get_columns():
 #     return [
@@ -61,45 +47,18 @@
 #         {"label": "Balance", "fieldname": "balance", "width": 120},
 #         {"label": "View", "fieldname": "view", "width": 80},
 #     ]
+def get_columns():
+    return [
+        {"label": "Month", "fieldname": "month_display", "width": 120},
+        {"label": "Target (Kg)", "fieldname": "target", "width": 140},
+        {"label": "Achieved (Kg)", "fieldname": "achieved", "width": 140},
+        {"label": "Balance", "fieldname": "balance", "width": 120},
+        {"label": "Achieved %", "fieldname": "achieved_percent", "width": 120},
+        {"label": "View", "fieldname": "view", "width": 80},
+    ]
 
 
-# # def get_data(year=None, project=None):
-# #     conditions = []
-# #     values = {}
 
-# #     if year:
-# #         conditions.append("mt.year = %(year)s")
-# #         values["year"] = year
-
-# #     if project:
-# #         conditions.append("ct.project_number = %(project)s")
-# #         values["project"] = project
-
-# #     where = " AND ".join(conditions)
-# #     if where:
-# #         where = "WHERE " + where
-
-# #     data = frappe.db.sql(f"""
-# #         SELECT
-# #             mt.select_month AS month_raw,
-# #             SUM(ct.total_weight_of_project) AS target,
-# #             COALESCE(SUM(ma.total_weight_for_project_achieve),0) AS achieved
-# #         FROM `tabFT Monthly Target` mt
-# #         INNER JOIN `tabFT Month Target Childtable` ct
-# #             ON ct.parent = mt.name
-# #         LEFT JOIN `tabFT Monthly Achievement` ma
-# #             ON ma.project_number = ct.project_number
-# #             AND ma.select_month = mt.name
-# #         {where}
-# #         GROUP BY mt.select_month
-# #     """, values, as_dict=True)
-
-# #     for d in data:
-# #         d.month_display = d.month_raw
-# #         d.balance = (d.target or 0) - (d.achieved or 0)
-# #         d.view = ""
-
-# #     return data
 # def get_data(year=None, project=None):
 #     conditions = []
 #     values = {}
@@ -122,13 +81,18 @@
 #             mt.select_month AS month_display,
 #             SUM(ct.total_weight_of_project) AS target,
 #             COALESCE(SUM(ma.total_weight_for_project_achieve), 0) AS achieved
+
 #         FROM `tabFT Monthly Target` mt
 #         INNER JOIN `tabFT Month Target Childtable` ct
 #             ON ct.parent = mt.name
+
 #         LEFT JOIN `tabFT Monthly Achievement` ma
 #             ON ma.project_number = ct.project_number
 #             AND ma.select_month = mt.name
+#             AND ma.year = mt.year
+
 #         {where}
+
 #         GROUP BY mt.name, mt.select_month
 #     """, values, as_dict=True)
 
@@ -137,110 +101,6 @@
 #         d["view"] = ""
 
 #     return data
-
-
-# # @frappe.whitelist()
-# # def get_month_details(year=None, project=None, month=None):
-# #     return frappe.db.sql("""
-# #         SELECT
-# #             ct.project_number AS project,
-# #             ct.total_weight_of_project AS target,
-# #             ma.total_weight_for_project_achieve AS achieved,
-# #             (ct.total_weight_of_project -
-# #              COALESCE(ma.total_weight_for_project_achieve,0)) AS balance,
-# #             ic.invoice_no,
-# #             ic.attach_file AS attachment
-# #         FROM `tabFT Monthly Target` mt
-# #         INNER JOIN `tabFT Month Target Childtable` ct
-# #             ON ct.parent = mt.name
-# #         LEFT JOIN `tabFT Monthly Achievement` ma
-# #             ON ma.project_number = ct.project_number
-# #             AND ma.select_month = mt.name
-# #         LEFT JOIN `tabFT Invoice Childtable` ic
-# #             ON ic.parent = ma.name
-# #         WHERE mt.year = %(year)s
-# #           AND mt.select_month = %(month)s
-# #           AND (%(project)s IS NULL OR ct.project_number = %(project)s)
-# #     """, {
-# #         "year": year,
-# #         "month": month,
-# #         "project": project
-# #     }, as_dict=True)
-# @frappe.whitelist()
-# def get_month_details(year=None, project=None, month=None):
-#     if not month:
-#         return []
-
-#     return frappe.db.sql("""
-#         SELECT
-#             ct.project_number AS project,
-#             ct.total_weight_of_project AS target,
-
-#             COALESCE(ma.total_weight_for_project_achieve, 0) AS achieved,
-
-#             (ct.total_weight_of_project -
-#              COALESCE(ma.total_weight_for_project_achieve, 0)) AS balance,
-
-#             ic.invoice_no,
-#             ic.attach_file AS attachment
-
-#         FROM `tabFT Monthly Target` mt
-
-#         INNER JOIN `tabFT Month Target Childtable` ct
-#             ON ct.parent = mt.name
-
-#         LEFT JOIN `tabFT Monthly Achievement` ma
-#             ON ma.project_number = ct.project_number
-#             AND ma.select_month = mt.name
-
-#         LEFT JOIN `tabFT Invoice Childtable` ic
-#             ON ic.parenttype = 'FT Monthly Achievement'
-#             AND ic.parent = ma.name
-
-#         WHERE mt.name = %(month)s
-#           AND mt.year = %(year)s
-#           AND (%(project)s IS NULL OR ct.project_number = %(project)s)
-#     """, {
-#         "year": year,
-#         "month": month,
-#         "project": project
-#     }, as_dict=True)
-
-# # /////////15-1-26
-
-
-
-
-
-
-import frappe
-def execute(filters=None):
-    filters = filters or {}
-    year = filters.get("year")
-    project = filters.get("project")
-
-    columns = get_columns()
-    data = get_data(year, project)
-
-    total_target = sum(d.get("target", 0) for d in data)
-    total_achieved = sum(d.get("achieved", 0) for d in data)
-
-    summary = [
-        {"label": "Total Target (Kg)", "value": total_target},
-        {"label": "Total Achieved (Kg)", "value": total_achieved},
-        {"label": "Balance", "value": total_target - total_achieved}
-    ]
-
-    return columns, data, None, None, summary
-
-def get_columns():
-    return [
-        {"label": "Month", "fieldname": "month_display", "width": 120},
-        {"label": "Target (Kg)", "fieldname": "target", "width": 140},
-        {"label": "Achieved (Kg)", "fieldname": "achieved", "width": 140},
-        {"label": "Balance", "fieldname": "balance", "width": 120},
-        {"label": "View", "fieldname": "view", "width": 80},
-    ]
 
 def get_data(year=None, project=None):
     conditions = []
@@ -261,7 +121,8 @@ def get_data(year=None, project=None):
     data = frappe.db.sql(f"""
         SELECT
             mt.name AS month_id,
-            mt.select_month AS month_display,
+            mt.select_month AS month_name,
+            mt.year AS month_year,
             SUM(ct.total_weight_of_project) AS target,
             COALESCE(SUM(ma.total_weight_for_project_achieve), 0) AS achieved
 
@@ -276,14 +137,29 @@ def get_data(year=None, project=None):
 
         {where}
 
-        GROUP BY mt.name, mt.select_month
+        GROUP BY mt.name, mt.select_month, mt.year
+        ORDER BY mt.year, FIELD(mt.select_month,
+            'January','February','March','April','May','June','July','August','September','October','November','December')
     """, values, as_dict=True)
 
     for d in data:
+        # Format month_display as "Mon-YY"
+        month_abbr = d["month_name"][:3]
+        year_short = str(d["month_year"])[-2:]
+        d["month_display"] = f"{month_abbr}-{year_short}"
+
+        # Calculate balance
         d["balance"] = (d["target"] or 0) - (d["achieved"] or 0)
+
+        # Calculate Achieved %
+        d["achieved_percent"] = 0
+        if d["target"]:
+            d["achieved_percent"] = round((d["achieved"] / d["target"]) * 100, 2)
+
         d["view"] = ""
 
     return data
+
 
 @frappe.whitelist()
 def get_month_details(year=None, project=None, month=None):
