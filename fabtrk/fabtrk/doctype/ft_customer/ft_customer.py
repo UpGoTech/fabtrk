3b4e6f197cebb1a54af9e3a96c092fe167983169
shubhangi831 <shubhangi099kamble@gmail.com>
2026-01-31 11:49:52 +0000

    Update

--- fabtrk/fabtrk/doctype/ft_customer/ft_customer.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, UpGo Technologies and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FTCustomer(Document):
-	pass
--- fabtrk/fabtrk/doctype/ft_project_type/test_ft_project_type.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, UpGo Technologies and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestFTProjectType(FrappeTestCase):
-	pass
