9284f1878f5561de0a87f5b99dbc053104afdf9f
mrunali00 <mrunal200026@gmail.com>
2026-02-07 11:08:50 +0000

    Add dawing , partdoctype ,show all data with Total Weight in report

--- fabtrk/fabtrk/doctype/ft_drawings/ft_drawings.js
+++ fabtrk/fabtrk/doctype/ft_drawings/ft_drawings.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2026, UpGo Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("FT Drawings", {
-// 	refresh(frm) {
+frappe.ui.form.on("FT Drawings", {
+    unit_weight(frm) {
+        calculate_total_weight(frm);
+    },
+    quantity(frm) {
+        calculate_total_weight(frm);
+    }
+});
 
-// 	},
-// });
+function calculate_total_weight(frm) {
+    let unit_weight = frm.doc.unit_weight || 0;
+    let quantity = frm.doc.quantity || 0;
+
+    frm.set_value("total_weight", unit_weight * quantity);
+}
+
