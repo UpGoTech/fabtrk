028f3d0f92bd38ea23bdeb854cbc79a5214b01da
shubhangi831 <shubhangi099kamble@gmail.com>
2026-02-13 08:03:37 +0000

    Merge Current Code

--- fabtrk/fabtrk/doctype/drawing_parts/drawing_parts.js
+++ fabtrk/fabtrk/doctype/drawing_parts/drawing_parts.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2026, UpGo Technologies and contributors
 // For license information, please see license.txt
 
+
+
 frappe.ui.form.on("Drawing Parts", {
     quantity(frm) {
         calculate_total(frm);
