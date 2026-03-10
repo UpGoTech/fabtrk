d080fa612a7dd99680aa65bde3b97aa92294235f
mrunali00 <mrunal200026@gmail.com>
2026-03-10 10:47:32 +0000

    remove unwanted code and filed,command

--- fabtrk/fabtrk/doctype/ft_add_drawing/ft_add_drawing.py
+++ fabtrk/fabtrk/doctype/ft_add_drawing/ft_add_drawing.py
@@ -1,29 +1,40 @@
-# # Copyright (c) 2026, UpGo Technologies and contributors
-# # For license information, please see license.txt
+# # # Copyright (c) 2026, UpGo Technologies and contributors
+# # # For license information, please see license.txt
 
-import frappe
+#  import frappe
 from frappe.model.document import Document
 
 
-# class FTAddDrawing(Document):
-# 	pass
-
-
 class FTAddDrawing(Document):
-    def validate(self):
-        self.validate_duplicate_drawing_per_project()
+	pass
 
-    def validate_duplicate_drawing_per_project(self):
-        exists = frappe.db.exists(
-            "Add Drawing",
-            {
-                "project_number": self.project_number,
-                "drawing_number": self.drawing_number,
-                "name": ["!=", self.name]  # update case handle
-            }
-        )
 
-        if exists:
-            frappe.throw(
-                f"Drawing Number <b>{self.drawing_number}</b> already exists for Project <b>{self.project_number}</b>"
-            )
+
+
+# import frappe
+# from frappe.model.document import Document
+
+
+# # class FTAddDrawing(Document):
+# # 	pass
+
+
+# class FTAddDrawing(Document):
+#     def validate(self):
+#         self.validate_duplicate_drawing_per_project()
+
+#     def validate_duplicate_drawing_per_project(self):
+#         exists = frappe.db.exists(
+#             "Add Drawing",
+#             {
+#                 "project_number": self.project_number,
+#                 "drawing_number": self.drawing_number,
+#                 "name": ["!=", self.name]  # update case handle
+#             }
+#         )
+
+#         if exists:
+#             frappe.throw(
+#                 f"Drawing Number <b>{self.drawing_number}</b> already exists for Project <b>{self.project_number}</b>"
+#             )
+
--- fabtrk/fabtrk/doctype/ft_add_drawing/ft_add_drawing_list.js
+++ fabtrk/fabtrk/doctype/ft_add_drawing/ft_add_drawing_list.js
@@ -1,16 +1,16 @@
-frappe.listview_settings["FT Add Drawing"] = {
-    formatters: {
-        unit_weight(value) {
-            if (value !== undefined && value !== null) {
-                return Number(value).toFixed(3);
-            }
-            return value;
-        },
-        total_weight(value) {
-            if (value !== undefined && value !== null) {
-                return Number(value).toFixed(3);
-            }
-            return value;
-        }
-    }
-};
+// frappe.listview_settings["FT Add Drawing"] = {
+//     formatters: {
+//         unit_weight(value) {
+//             if (value !== undefined && value !== null) {
+//                 return Number(value).toFixed(3);
+//             }
+//             return value;
+//         },
+//         total_weight(value) {
+//             if (value !== undefined && value !== null) {
+//                 return Number(value).toFixed(3);
+//             }
+//             return value;
+//         }
+//     }
+// };
