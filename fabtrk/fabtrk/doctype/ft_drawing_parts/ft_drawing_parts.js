4f33bade0b820759ccaa48728d61cd85ae09f5b0
mrunali00 <mrunal200026@gmail.com>
2026-04-13 08:19:43 +0000

    merge: integrate feature-1 into develop with major refactor and new modules
    - Merged origin/feature-1 into develop branch
    - Refactored ft_drawing_parts and related logic
    - Added new Doctypes: ft_project_stages, ft_project_stages_child, ft_stages_master, ft_transaction
    - Added transaction dashboard page
    - Refactored and optimized reports (ft_drawing_part, ft_drawings, ft_transaction)
    - Removed deprecated reports (ft_drawing_report, ft_drawing_report_2, ft_drawing_part_report_2, project_monthly_report)
    - Updated stock and transaction related modules

--- fabtrk/fabtrk/doctype/ft_drawing_parts/ft_drawing_parts.js
+++ fabtrk/fabtrk/doctype/ft_drawing_parts/ft_drawing_parts.js
@@ -1,6 +1,5 @@
 // // Copyright (c) 2026, UpGo Technologies and contributors
 // // For license information, please see license.txt
-
 frappe.ui.form.on("FT Drawing Parts", {
     quantity(frm) {
         calculate_total(frm);
